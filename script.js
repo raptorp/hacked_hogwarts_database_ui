a41d15c695c9d927a73ae6cae6f1526a40d45855
Sabrina OS <sabrinaotelia@hotmail.com>
2023-02-22 16:35:53 +0100

    cleaned and prepped json data

---
+++ script.js
@@ -0,0 +1,40 @@
+fetch("https://petlatkea.dk/2021/hogwarts/students.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const cleanData = data.map((item) => {
+      const fullname = item.fullname.trim();
+      const house = item.house.trim().toLowerCase();
+      const gender = item.gender.trim().toLowerCase();
+      const [firstName, ...middleAndLastName] = fullname.split(" ");
+      let lastName = "";
+      let middleName = "";
+      let nickname = fullname.match(/\"(.*?)\"/)?.[1];
+
+      if (middleAndLastName.length === 0) {
+        lastName = "";
+      } else {
+        lastName = middleAndLastName.pop();
+      }
+
+      // remove special characters from the name properties
+      const cleanFirstName = firstName
+        .replace(/[^a-zA-Z\-]/g, "")
+        .toLowerCase();
+      const cleanLastName = lastName.replace(/[^a-zA-Z\-]/g, "").toLowerCase();
+      const cleanMiddleName = middleName
+        .replace(/[^a-zA-Z\-]/g, "")
+        .toLowerCase();
+
+      return {
+        firstName: cleanFirstName,
+        lastName: cleanLastName,
+        middleName: cleanMiddleName,
+        nickname,
+        house: house.toLowerCase(),
+        gender: gender.toLowerCase(),
+      };
+    });
+
+    console.log(cleanData);
+  })
+  .catch((error) => console.error(error));
