023338bcad432a8886417616e8d31eacf1607c74
Sabrina OS <sabrinaotelia@hotmail.com>
2023-02-23 13:02:33 +0100

    added js to display student data

--- script.js
+++ script.js
@@ -16,6 +16,9 @@ fetch("https://petlatkea.dk/2021/hogwarts/students.json")
         lastName = "";
       } else {
         lastName = middleAndLastName.pop();
+        if (middleAndLastName.length > 0) {
+          middleName = middleAndLastName.join(" ");
+        }
       }
 
       // remove "special" characters from the name properties
@@ -35,6 +38,36 @@ fetch("https://petlatkea.dk/2021/hogwarts/students.json")
         house: house.toLowerCase(),
         gender: gender.toLowerCase(),
       };
+    });
+    // create table rows
+    const tableBody = document.querySelector("#studentTable tbody");
+    cleanData.forEach((student) => {
+      const row = document.createElement("tr");
+      const firstNameCell = document.createElement("td");
+      firstNameCell.textContent = student.firstName;
+      row.appendChild(firstNameCell);
+
+      const lastNameCell = document.createElement("td");
+      lastNameCell.textContent = student.lastName;
+      row.appendChild(lastNameCell);
+
+      const middleNameCell = document.createElement("td");
+      middleNameCell.textContent = student.middleName;
+      row.appendChild(middleNameCell);
+
+      const nicknameCell = document.createElement("td");
+      nicknameCell.textContent = student.nickname || "-";
+      row.appendChild(nicknameCell);
+
+      const houseCell = document.createElement("td");
+      houseCell.textContent = student.house;
+      row.appendChild(houseCell);
+
+      const genderCell = document.createElement("td");
+      genderCell.textContent = student.gender;
+      row.appendChild(genderCell);
+
+      tableBody.appendChild(row);
     });
 
     console.log(cleanData);
